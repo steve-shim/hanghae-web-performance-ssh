62b4027ec172319a0964e3fdb16eec6e1a88ebb0
SteveShim <steveshim496@gmail.com>
2024-08-13 21:21:47 +0900

    pervent layout shift of country banner - 2

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
-function showTopBar(){
-    let country = "France";
-    let vat = 20;
-    setTimeout(() => {
-        document.querySelector("section.country-bar")
-        .innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`
-        document.querySelector("section.country-bar")
-        .classList.remove('hidden')
-    }, 1000);
+function showTopBar() {
+  let country = "France";
+  let vat = 20;
+  setTimeout(() => {
+    document.querySelector("section.country-bar").innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
+    document.querySelector("section.country-bar");
+  }, 1000);
 }
 
-
-
 showTopBar();
-
-
-
-
